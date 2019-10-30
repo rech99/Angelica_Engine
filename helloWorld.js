5132845de9094927ce1213a42587fa1f26ffb9f2
rech99 <rech_99@hotmail.com>
2019-10-30 09:31:07 -0700

    cambios

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,22 @@
 const canvas = document.getElementById('glcanvas');
 
+const vsSource = `
+    attribute vec4 aVertexPosition;
+
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+  `;
+
+  const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
+
 const main = ()=>{
     const gl = canvas.getContext('webgl');
 
